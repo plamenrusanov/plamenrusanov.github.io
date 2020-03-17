e592e5c2d441b99dc0cf7c1e9b8016aa57091c0c
Plamen <prusanov@students.softuni.bg>
2020-03-17 14:17:01 +0200

    Seed Roles

--- Tapas.Common/GlobalConstants.cs
+++ Tapas.Common/GlobalConstants.cs
@@ -5,5 +5,11 @@
         public const string SystemName = "Tapas";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string OperatorRoleName = "Operator";
+
+        public const string UserRoleName = "User";
+
+        public const string EmailAdministrator = "plamen.rusanov@abv.bg";
     }
 }
