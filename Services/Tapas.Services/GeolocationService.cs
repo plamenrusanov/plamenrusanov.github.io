32437498b58bd8778f6e5efd3cfe17b7e9f8b61d
Plamen <prusanov@students.softuni.bg>
2020-04-08 09:14:06 +0300

    Geolocation use compresion

--- Services/Tapas.Services/GeolocationService.cs
+++ Services/Tapas.Services/GeolocationService.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tapas.Services
 {
+    using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Threading.Tasks;
@@ -19,10 +20,16 @@
 
         public async Task<PositionDto> GetAddressAsync(string latitude, string longitude)
         {
-            using (var client = new HttpClient())
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip,
+            };
+
+            using (var client = new HttpClient(handler))
             {
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("bg", 0.8));
+                client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
 
                 var result = await client.GetAsync($"https://eu1.locationiq.com/v1/reverse.php?key={this.apiKey}&lat={latitude}&lon={longitude}&format=json");
                 var resultContent = await result.Content.ReadAsStringAsync();
