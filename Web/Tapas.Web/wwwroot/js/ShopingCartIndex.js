ad2cc4e9dc9d5ac7afa8c431932230326586af9d
Plamen <prusanov@students.softuni.bg>
2020-05-15 10:36:14 +0300

    Refactoring

---
+++ Web/Tapas.Web/wwwroot/js/ShopingCartIndex.js
@@ -0,0 +1,30 @@
+﻿
+function SetDescription() {
+    var itemId = document.getElementById("sciId").value;
+    var message = document.getElementById("message-text").value;
+    $.ajax({
+        url: `/ShopingCart/SetDescription?id=${itemId}&message=${message}`,
+        success: function (responce) {
+            var el = document.getElementById("closeModal");
+            el.click();
+        }
+
+    });
+}
+
+
+function GetDescription(itemId) {
+    $.ajax({
+        url: `/ShopingCart/GetDescription?id=${itemId}`,
+        success: function (response) {
+            var el = document.getElementById(`button${itemId}`);
+            el.click();
+            var elem = document.getElementById("message-text");
+            elem.value = response;
+            var productName = document.getElementById(`name${itemId}`).innerText;
+            document.getElementById("exampleModalLongTitle").innerText = productName;
+            var sciId = document.getElementById("sciId");
+            sciId.value = itemId;
+        }
+    });
+}
