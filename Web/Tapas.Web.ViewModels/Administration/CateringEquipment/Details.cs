6c34fa593b2a2dd05d1487b5d1ae4052f5d77c14
Plamen <prusanov@students.softuni.bg>
2020-06-02 17:49:39 +0300

    Details Equipment Names

--- Web/Tapas.Web.ViewModels/Administration/CateringEquipment/Details.cs
+++ Web/Tapas.Web.ViewModels/Administration/CateringEquipment/Details.cs
@@ -1,15 +1,20 @@
 ﻿namespace Tapas.Web.ViewModels.Administration.CateringEquipment
 {
+    using System.ComponentModel;
+
     public class Details
     {
         public string Id { get; set; }
 
+        [DisplayName("Име")]
         public string Name { get; set; }
 
         public string ImageUrl { get; set; }
 
+        [DisplayName("Описание")]
         public string Description { get; set; }
 
+        [DisplayName("Цена")]
         public decimal Price { get; set; }
     }
 }
